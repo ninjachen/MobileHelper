1d95cb6638a3951a2cf66fb4c24dd84c2b3a2b39
jiachenning <jiachenning@gmail.com>
2014-03-26 01:31:28 +0800

    doing import function

--- app/src/main/java/com/ninja/mobilehelper/MainActivity.java
+++ app/src/main/java/com/ninja/mobilehelper/MainActivity.java
@@ -1,8 +1,9 @@
 package com.ninja.mobilehelper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -13,12 +14,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import com.google.gson.Gson;
+import org.apache.http.util.EncodingUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class MainActivity extends ListActivity {
@@ -80,25 +80,31 @@ public class MainActivity extends ListActivity {
         } else if (id == R.id.export_contacts) {
             export_contacts();
         } else if (id == R.id.import_contacts) {
-
+            selectFile();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean import_contacts() {
-//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//        intent.setType("*/*");
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        try {
-//            startActivityForResult(Intent.createChooser(intent, "请选择一个要上传的文件"),
-//                    FILE_SELECT_CODE);
-//        } catch (android.content.ActivityNotFoundException ex) {
-//            // Potentially direct the user to the Market with a Dialog
-//            Toast.makeText(MainActivity.this, "plz install file explorer", Toast.LENGTH_SHORT)
-//                    .show();
-//        }
+    public boolean selectFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "请选择一个联系人备份文件"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(MainActivity.this, "plz install file explorer", Toast.LENGTH_SHORT)
+                    .show();
+        }
         return true;
     }
+
+    public boolean import_contacts() {
+        //todo
+        return true;
+    }
+
 
     public boolean export_contacts() {
         if (contactsList != null && contactsList.size() > 0) {
@@ -112,13 +118,31 @@ public class MainActivity extends ListActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
+        //import contacts
         if (resultCode == Activity.RESULT_OK
                 && requestCode == FILE_SELECT_CODE) {
             // Get the Uri of the selected file
             Uri uri = data.getData();
             String url = uri.toString();
 
+            String contacts = null;
+            try {
+                contacts = readFile(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Gson gson = new Gson();
+            ArrayList<String> contactList = gson.fromJson(contacts, ArrayList.class);
+            new AlertDialog.Builder(this)
+                    .setMessage("Are you sure " + contactList.size() +" contacts will be import?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            import_contacts();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
             Log.i(TAG, "uri" + uri);
 
         }
@@ -159,5 +183,27 @@ public class MainActivity extends ListActivity {
         catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
+    }
+
+    //读SD中的文件
+    public String readFile(String filePath) throws IOException{
+        String res="";
+        try{
+            FileInputStream fin = new FileInputStream(filePath);
+
+            int length = fin.available();
+
+            byte [] buffer = new byte[length];
+            fin.read(buffer);
+
+            res = EncodingUtils.getString(buffer, "UTF-8");
+
+            fin.close();
+        }
+
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return res;
     }
 }
